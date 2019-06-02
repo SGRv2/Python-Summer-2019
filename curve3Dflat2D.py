2b06863226e5875fe2d6c9a76be9e4e2aa0bb8bb
SGRv2 <43145961+SGRv2@users.noreply.github.com>
2019-06-02 18:28:19 +0530

    Update curve3Dflat2D.py

--- curve3Dflat2D.py
+++ curve3Dflat2D.py
@@ -9,8 +9,8 @@ import matplotlib.pyplot as plt
 
 # using cv2 to read the image  
 img = cv2.imread('picklejar.jpg')
-gray_image = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-ret, thresh = cv2.threshold(gray_image,127,255,cv2.THRESH_BINARY)
+grayimg = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+ret, thresh = cv2.threshold(grayimg,127,255,cv2.THRESH_BINARY)
 edges = cv2.Canny(thresh ,100, 200)
 
 # Find largest contour (should be the label)
@@ -33,36 +33,38 @@ right=cv2.Sobel(mask,ddepth,1,0,ksize=1,scale=-1,delta=0, borderType)
 top=cv2.Sobel(mask,ddepth,0,1,ksize=1,scale=1,delta=0,borderType)
 bottom=cv2.Sobel(mask,ddepth,0,1,ksize=1,scale=-1,delta=0,borderType)
 
-# Noise removal
+# remove noise from borders
 kernel = np.ones((2,2),np.uint8)
-left_border = cv2.erode(left,kernel,iterations = 1)
-right_border = cv2.erode(right,kernel,iterations = 1)
-top_border = cv2.erode(top,kernel,iterations = 1)
-bottom_border = cv2.erode(bottom,kernel,iterations = 1)
+leftb = cv2.erode(left,kernel,iterations = 1)
+rightb = cv2.erode(right,kernel,iterations = 1)
+topb = cv2.erode(top,kernel,iterations = 1)
+bottomb = cv2.erode(bottom,kernel,iterations = 1)
 
-# Find coeficients c1,c2, ... ,c7,c8 by minimizing the error function. 
-# Points on the left border should be mapped to (0,anything).
-# Points on the right border should be mapped to (108,anything)
-# Points on the top border should be mapped to (anything,0)
-# Points on the bottom border should be mapped to (anything,70)
-# Equations 1 and 2: 
-#    c1 + c2*x + c3*y + c4*x*y, c5 + c6*y + c7*x + c8*x^2
+'''
+Find coefficients c1,c2, ... ,c7,c8 by minimizing the error function. 
+Points on the left border should be mapped to (0,anything).
+Points on the right border should be mapped to (108,anything)
+Points on the top border should be mapped to (anything,0)
+Points on the bottom border should be mapped to (anything,70)
+ Equations 1 and 2: 
+    c1 + c2*x + c3*y + c4*x*y, c5 + c6*y + c7*x + c8*x^2
+'''
 
 sumOfSquares_y = '+'.join(["(c[0]+c[1]*%s+c[2]*%s+c[3]*%s*%s)**2" %
-    (x,y,x,y) for y,x,z in np.transpose(np.nonzero(left_border)) ])
+    (x,y,x,y) for y,x,z in np.transpose(np.nonzero(leftb)) ])
 sumOfSquares_y += " + "
 sumOfSquares_y += \
     '+'.join(["(-108+c[0]+c[1]*%s+c[2]*%s+c[3]*%s*%s)**2" % \
-    (x,y,x,y) for y,x,z in np.transpose(np.nonzero(right_border)) ])
+    (x,y,x,y) for y,x,z in np.transpose(np.nonzero(rightb)) ])
 res_y = optimize.minimize(lambda c: eval(sumOfSquares_y),(0,0,0,0),method='SLSQP')
 
 sumOfSquares_x = \
     '+'.join(["(-70+c[0]+c[1]*%s+c[2]*%s+c[3]*%s*%s)**2" % \
-    (y,x,x,x) for y,x,z in np.transpose(np.nonzero(bottom_border))])
+    (y,x,x,x) for y,x,z in np.transpose(np.nonzero(bottomb))])
 sumOfSquares_x += " + "
 sumOfSquares_x += \
     '+'.join( [ "(c[0]+c[1]*%s+c[2]*%s+c[3]*%s*%s)**2" % \
-    (y,x,x,x) for y,x,z in np.transpose(np.nonzero(top_border)) ] )
+    (y,x,x,x) for y,x,z in np.transpose(np.nonzero(topb)) ] )
 res_x = optimize.minimize(lambda c: eval(sumOfSquares_x),(0,0,0,0), method='SLSQP')
 
 
@@ -90,20 +92,21 @@ bottommost = tuple(cnt[cnt[:,:,1].argmax()][0])
 
 dst = list()
 src = list()
-for y,x,z in np.transpose(np.nonzero(top_border)):
+for y,x,z in np.transpose(np.nonzero(topb)):
     dst.append([x,y])
     src.append([x,topmost[1]])
-for y,x,z in np.transpose(np.nonzero(bottom_border)):
+for y,x,z in np.transpose(np.nonzero(bottomb)):
     dst.append([x,y])
     src.append([x,bottommost[1]])
-for y,x,z in np.transpose(np.nonzero(left_border)):
+for y,x,z in np.transpose(np.nonzero(leftb)):
     dst.append([x,y])
     src.append([leftmost[0],y])
-for y,x,z in np.transpose(np.nonzero(right_border)):
+for y,x,z in np.transpose(np.nonzero(rightb)):
     dst.append([x,y])
     src.append([rightmost[0],y])
 src = np.array(src)
 dst = np.array(dst)
+
 
 tform3 = tf.PiecewiseAffineTransform()
 tform3.estimate(src, dst)
